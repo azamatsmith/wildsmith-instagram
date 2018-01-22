3afd407e601d3b22dde42998373450e400cf9723
Matthew Smith <azamatsmith@gmail.com>
2018-01-22 16:03:54 -0700

    Only use dotenv when not in production

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Init env variables
-require('dotenv').config();
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
