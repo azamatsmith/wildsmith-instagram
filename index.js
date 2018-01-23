056a035ae1cc74a3fff76c02f6b9c7732f485e05
Matthew Smith <azamatsmith@gmail.com>
2018-01-22 21:58:49 -0700

    Add cors lib

--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const port = process.env.PORT || 3746;
 const schema = require('./schema');
+const cors = require('cors');
 
 const app = express();
 
 // Allow CORS
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+app.use(cors())
 
 app.use(
   '/graphql',
