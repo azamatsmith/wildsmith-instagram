76449b2835dccc650e39e07dc938360bca9f6a72
Matthew Smith <azamatsmith@gmail.com>
2018-01-22 21:54:25 -0700

    Allow CORS

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const schema = require('./schema');
 
 const app = express();
 
+// Allow CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(
   '/graphql',
   graphqlHTTP({
