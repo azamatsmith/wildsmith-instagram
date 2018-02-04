21a6d601177a2d7d118a465767c3c6d46d374428
Matthew Smith <azamatsmith@gmail.com>
2018-02-04 11:00:48 -0700

    Add contact endpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // Init env variables
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
+const axios = require('axios');
+const bodyParser = require('body-parser');
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const port = process.env.PORT || 3746;
@@ -12,6 +14,12 @@ const app = express();
 // Allow CORS
 app.use(cors())
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+//
+// // parse application/json
+app.use(bodyParser.json())
+
 app.use(
   '/graphql',
   graphqlHTTP({
@@ -19,6 +27,16 @@ app.use(
     graphiql: true,
   })
 );
+
+app.post('/contact', (req, res) => {
+  const {body} = req;
+  axios.post(process.env.ZAPIER_URL, body)
+  .then(() => res.json({ success: true}))
+  .catch((err) => {
+    console.log('error', err);
+    res.json({ success: false, error: true})
+  });
+});
 
 app.listen(port, () => {
   console.log('listening on port', port);
